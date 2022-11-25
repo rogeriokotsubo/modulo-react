218fd1073b6e4ee9b90a83af4d087d016e296b4d
Rogerio Kotsubo <rogeriokotsubo@gmail.com>
2022-11-25 13:45:59 -0300

    feat: aula2

--- app-aula1/src/App.tsx
+++ app-aula1/src/App.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from './components/button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface iUser {
+  firstName: string,
+  lastName: string,
+  birthYear: number,
+  currentYear: number
 }
 
-export default App;
+function formatName(user : iUser)
+{
+  return ` meu nome é ${user.firstName} ${user.lastName}, tenho ${user.currentYear-user.birthYear} anos e este é 
+        meu primeiro contato com JSX`;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Kool',
+  birthYear: 1997,
+  currentYear: 2022
+}
+
+export default function Element() {
+  const element = (
+      <h1>
+        Olá, {formatName(user)}!
+      </h1>
+  );
+  return element;
+}
+
+
+
